2af9ccac17516808602ad044d15eb791e338f4d2
root <root@python.(none)>
2016-04-07 13:50:03 -0400

    this is the unicode test

---
+++ 47/unic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+kel = u'中文'
+some ='this is a asc ii character'
+
+print kel + some
+
+other = '中文'
+print kel + unicode(other,'utf-8')
+print kel + other
