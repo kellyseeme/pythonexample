70ca85b3a953b21a61e679b91068eeafe127ec13
root <root@python.(none)>
2016-05-09 06:00:19 -0400

    this is the paramiko examples

---
+++ 58/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class kel(object):
+    def __init__(self):
+        pass
+
+def some():
+    return kel()
+
+
+if some():
+    print 'object is True'
+else:
+    print 'object is None??'
