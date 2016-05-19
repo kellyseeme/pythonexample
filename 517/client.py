474411bd7b96ded650a9ead72dd997b723b6b9e4
root <root@python.(none)>
2016-05-19 12:41:21 -0400

    this is a ftp examples

---
+++ 517/client.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import socket
+import time
+import progressbar
+
+HOST = '192.168.1.60'
+PORT = 9999
+
+def recv_all(socketobj,file_name,file_size):
+    print file_size
+    f = open(file_name,'w')
+    bar = progressbar.ProgressBar(total=10)
+    while file_size > 0:
+        if file_size <= 1024:
+            data = socketobj.recv(1024)
+            f.write(data)
+            break
+        elif file_size > 1024:
+            bar.move()
+            bar.log()
+            data = socketobj.recv(1024)
+            f.write(data)
+            file_size -= 1024
+    f.close()
+s = socket.socket()
+s.connect((HOST,PORT))
+while True:
+    commands = raw_input('>>>')
+    if commands == 'exit' or not commands:break
+    s.sendall(commands)
+    options = commands.strip().split(' ')
+    if len(options) == 2:
+        file_name = options[1]
+        if options[0] == 'put':
+            f = open(file_name)
+            data = f.read()
+            time.sleep(0.2)
+            s.send(str(len(data)))
+            bar = progressbar.ProgressBar(total=len(data)/(1024*1024)+1)
+            bar.move()
+            bar.log()
+            time.sleep(0.2)
+            s.send(data)
+            print s.recv(1024)
+        elif options[0] == 'get':
+            file_size = int(s.recv(1024))
+            recv_all(s,file_name,file_size)
+            print s.recv(1024)
+    else:
+        pass
